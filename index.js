7362958de95b318fd5ac86fd75ca50e9f5879991
weichichou <weichi.cc@gmail.com>
2019-11-12 17:31:41 +0100

    initialize express API

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+app.use(bodyParser.json())
+
+const port = 4000
+app.listen(port, () => console.log(`Listen on port ${port}`))
