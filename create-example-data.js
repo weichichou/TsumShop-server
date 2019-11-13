54458d6218a4d0dc7b416a414696a5b75cccec59
weichichou <weichi.cc@gmail.com>
2019-11-13 09:43:50 +0100

    refactor db.sync

--- create-example-data.js
+++ create-example-data.js
@@ -7,7 +7,7 @@ User
             username: 'testuser',
             email: 'test@test.com',
             phone: '1234567',
-            password: 'abcdefg'
+            password: 'abcdefg',
         })
     })
     .catch((err) => console.error(err))
@@ -19,8 +19,9 @@ Ad
             title: 'Jack',
             desc: 'Holiday Jack Tsum has a unique Timed Skill. He will automatically clear chains of Tsums. His base time is maxed at 3 seconds.',
             price: 150,
-            pictureUrl: 'https://vignette.wikia.nocookie.net/disneytsumtsum/images/6/67/SantaJack.png/revision/latest?cb=20150118005419'    
+            pictureUrl: 'https://vignette.wikia.nocookie.net/disneytsumtsum/images/6/67/SantaJack.png/revision/latest?cb=20150118005419',
+            userId: 1    
         })
     })
     .catch((err)=>console.error(err))
-    
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const db = require('./db')
+const {db} = require('./db')
 
 const app = express()
 const corsMiddleware = cors()
